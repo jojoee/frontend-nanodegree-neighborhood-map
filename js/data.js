e411fdb592593d0c4902bbe9f4bed3ef962faa1f
Nathachai Thongniran <inid3a@gmail.com>
2016-04-29 17:36:25 +0700

    Update initial data

--- js/data.js
+++ js/data.js
@@ -2,9 +2,73 @@
 
 var initialLocations = [
   {
-    name: 'Test',
-    lat: 1,
-    lng: 2,
-    desc: ''
+    name: 'Siam Paragon',
+    lat: 13.746709,
+    lng: 100.534939,
+    type: 'mall',
+    desc: 'is a shopping mall in Bangkok, Thailand. It is one of the biggest shopping centres in Asia. Opened on December 9, 2005, it includes a wide range of specialty stores and restaurants as well as a multiplex movie theatre (consisted of 15 large size theaters with one of them having the biggest screen and seating capacity in Asia) and the Sea Life Ocean World (Underwater world) aquarium (the largest aquarium in South East Asia) and an exhibition hall and the Thai Art Gallery and also an opera concert hall. It also has a large bowling alley and karaoke centre. It is a joint venture by Siam Piwat, the company that owns the adjacent Siam Center/Siam Discovery shopping malls, and The Mall Group, which also owns The Emporium. Siam Paragon has attracted large crowds since it opened, but financial results aren\'t reported by the privately held Siam Paragon Development.'
+  },
+  {
+    name: 'Central World',
+    lat: 13.746736,
+    lng: 100.539241,
+    type: 'mall',
+    desc: 'is a shopping plaza and complex in Bangkok, Thailand. It is the sixth largest shopping complex in the world. The complex, which includes a hotel and office tower, is owned by Central Pattana. In 2006, after three years of design and renovation, CentralWorld was expanded to 550,000 square metres of shopping mall and 1,024,000 square metres of complex, topping nearby rival Siam Paragon in terms of size.'
+  },
+  {
+    name: 'BTS - Siam',
+    lat: 13.745517,
+    lng: 100.534675,
+    type: 'bts',
+    desc: 'Siam BTS subway station'
+  },
+  {
+    name: 'Siam Center',
+    lat: 13.746226,
+    lng: 100.532756,
+    type: 'mall',
+    desc: 'is a shopping center in Bangkok, Thailand. It was opened in 1973.'
+  },
+  {
+    name: 'BTS - National Stadium',
+    lat: 13.746436,
+    lng: 100.529152,
+    type: 'bts',
+    desc: 'National Stadium BTS subway station'
+  },
+  {
+    name: 'MBK Center',
+    lat: 13.744316,
+    lng: 100.529994,
+    type: 'mall',
+    desc: 'also known as Mahboonkrong (Thai: มาบุญครอง), is a large shopping mall in Bangkok, Thailand. At eight stories high, the center contains around 2,000 shops, restaurants and service outlets, including the 4-story Tokyu department store.'
+  },
+  {
+    name: 'Pantip Plaza',
+    lat: 13.750537,
+    lng: 100.537370,
+    type: 'mall',
+    desc: 'is an indoor IT shopping mall located on New Phetchaburi Road in Ratchathewi district, Bangkok, Thailand. It has satellite locations in Nonthaburi, on Bangkapi Road, and in Chiang Mai.'
+  },
+  {
+    name: 'BTS - Chit Lom',
+    lat: 13.744093,
+    lng: 100.543050,
+    type: 'bts',
+    desc: 'Chit Lom BTS subway station'
+  },
+  {
+    name: 'The Platinum Fashion Mall',
+    lat: 13.750034,
+    lng:  100.539682,
+    type: 'mall',
+    desc: 'is a 5 billion baht (100 million euro) shopping mall in Pratunam, Bangkok, Thailand, which specializes in fashion clothes and accessories retail and wholesale. It is a six-storey complex with an 11-storey condominium building above the complex.'
+  },
+  {
+    name: 'BTS - Ratchathewi',
+    lat: 13.751658,
+    lng: 100.531555,
+    type: 'bts',
+    desc: 'Ratchathewi BTS subway station'
   }
 ];
